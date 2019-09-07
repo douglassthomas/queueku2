23412b097c4f19c6e3492fb8de5611ecd081f1ca
douglassthomas <dougitu@gmail.com>
2019-09-07 20:56:23 +0700

    ehe

--- app/src/main/java/edu/bluejack182/queueku/Home.java
+++ app/src/main/java/edu/bluejack182/queueku/Home.java
@@ -1,24 +1,59 @@
 package edu.bluejack182.queueku;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class Home extends AppCompatActivity {
 
+    FirebaseAuth auth;
     FirebaseUser user;
+    TextView txtStatus;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        getIntent();
+        txtStatus = findViewById(R.id.txtStatus);
+        auth = FirebaseAuth.getInstance();
+
+        user = auth.getCurrentUser();
+
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("user-role").child("Bflki66wbzX6JXdeiV6hoDdcMn72");
+//        ValueEventListener vel = new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                String s = dataSnapshot.getValue(String.class);
+//                txtStatus.setText(s);
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        };
+//        myRef.addValueEventListener(vel);
+
+        Intent i = getIntent();
+        txtStatus.setText(user.getUid());
     }
 
     public void signout(View view) {
